b764fef7fce4ab90f79ce459839ad937847ae63f
Raghav Sai Cheedalla <raghavsaicheedalla@Raghavs-MacBook-Pro.local>
2015-07-18 10:48:37 -0700

    Initial Commit

---
+++ ScribblingPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScribblingPad
+//
+//  Created by Raghav Sai Cheedalla on 7/18/15.
+//  Copyright (c) 2015 Raghav Sai Cheedalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScribblingPad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScribblingPad
+//
+//  Created by Raghav Sai Cheedalla on 7/18/15.
+//  Copyright (c) 2015 Raghav Sai Cheedalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
